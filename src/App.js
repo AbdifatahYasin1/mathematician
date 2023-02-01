caad1bcfed1568d3d5351ea8beb2fc8481ddf1c5
AbdifatahYasin1 <iskatag123@gmail.com>
2023-02-01 12:26:10 +0300

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/calculator/*" element={<Calculator />} />
+          <Route exact path="/quote/*" element={<Quote />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
